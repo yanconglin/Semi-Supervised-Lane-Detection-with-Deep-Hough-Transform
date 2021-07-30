f9fdddda1cad3ea94b99be8fe294172e10a6e641
CVisioner <yanconglin@users.noreply.github.com>
2021-07-30 13:56:52 +0200

    Update train_erfnet.sh

--- train_erfnet.sh
+++ train_erfnet.sh
@@ -1,7 +1,7 @@
 python3 -u train_erfnet.py CULane ERFNet train_gt val_gt \
                         --lr 0.01 \
                         --gpus 0 \
-                        --resume /tudelft.net/staff-bulk/ewi/insy/VisionLab/yanconglin/lanes/ERFNet-CULane-HTIHT-scratch/5513873/trained/_erfnet_model_best.pth.tar \
+                        --resume /supervised_erfnet_model_best_1.pth.tar \
                         -j 4 \
                         -b 14 \
                         --epochs 12 \
